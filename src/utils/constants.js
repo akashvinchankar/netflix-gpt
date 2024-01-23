b43480e488e6efe5e22e37c803375025bc3cea07
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-23 13:23:33 +0530

    TMDB Api Integration, movies redux store setup, custom movie fetching hook

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,12 @@ export const BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const USER_LOGO = "https://avatars.githubusercontent.com/u/86399324?v=4";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWFjY2MyMjFjOTQwNDQ3ZDU2MDAwNjMxYWU3ZDc2MiIsInN1YiI6IjYxZmRmODlmOTFmMGVhMDA2NTZlZGRjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3bLu_gZ9Rt4b-ijetDJPlHn5S6nxlcRlcXji_DBms9o",
+  },
+};
