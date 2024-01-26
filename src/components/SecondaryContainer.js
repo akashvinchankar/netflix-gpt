10cc14f4c27c3f28320a9ce7058d148d0fb538d7
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-26 23:33:18 +0530

    Refactor SecondaryContainer component

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,15 +1,20 @@
 import React from "react";
-import MovieList from "./MovieList";
 import { useSelector } from "react-redux";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
   const movies = useSelector((state) => state.movies);
+
+  useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div className="bg-black">
       <div className="-mt-32 relative z-20">
         <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies?.popularMovies} />
       </div>
     </div>
   );
