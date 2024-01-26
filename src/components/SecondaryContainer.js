41421e273718ebd036d7a1a3f7a9c2f3504cf81c
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-26 23:38:43 +0530

    Add useUpcomingMovies hook and display upcoming movies

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -3,18 +3,21 @@ import { useSelector } from "react-redux";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MovieList from "./MovieList";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const SecondaryContainer = () => {
   const movies = useSelector((state) => state.movies);
 
   useNowPlayingMovies();
   usePopularMovies();
+  useUpcomingMovies();
 
   return (
     <div className="bg-black">
       <div className="-mt-32 relative z-20">
         <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
         <MovieList title={"Popular"} movies={movies?.popularMovies} />
+        <MovieList title={"Upcoming"} movies={movies?.upcomingMovies} />
       </div>
     </div>
   );
