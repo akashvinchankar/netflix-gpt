7069208b1e36f72db691fadfcf00e90e72e5b79f
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-26 20:33:00 +0530

    Add MovieList component and update SecondaryContainer

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((state) => state.movies);
+
+  return (
+    <div className="bg-black">
+      <div className="-mt-32 relative z-20">
+        <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+      </div>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
