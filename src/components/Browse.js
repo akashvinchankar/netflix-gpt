0dba2c8409c18077f40da5698e0a6346710b28ac
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-26 11:32:18 +0530

    Added Main Landing page setup

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
@@ -8,6 +10,16 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/* 
+      - Main Container
+        - Background Video
+        - Video Title
+      -Secondary Container
+        - Video List * n
+          - Movie Cards * n 
+      */}
     </div>
   );
 };
