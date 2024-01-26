88647c95dfc21f9d7d615d5ddea1d894e4783196
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-26 20:33:10 +0530

    Add MovieCard component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { IMG_CDN_URL } from "../utils/constants";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="w-56 pr-4 relative overflow-hidden group">
+      <img
+        className="rounded-lg transition-transform transform-gpu duration-300 group-hover:scale-105"
+        src={IMG_CDN_URL + movie.poster_path}
+        alt={movie.title}
+      />
+      <div className="absolute bottom-0 left-0 w-full p-4 opacity-0 bg-black bg-opacity-70 transition-opacity duration-300 group-hover:opacity-100">
+        <h1 className="text-white text-lg font-semibold mb-2">{movie.title}</h1>
+        <div className="flex flex-col text-white">
+          <p>Popularity: {movie.popularity}</p>
+          <p>Rating: {Math.round(movie.vote_average * 100) / 100}</p>
+          <p>Release Date: {movie.release_date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
