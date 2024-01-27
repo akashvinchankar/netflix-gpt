e193185feffbd0c2fc516bbc077d6036a36da031
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-27 14:11:13 +0530

    Refactor VideoTitle component to use language constants

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { searchLanguageConstants } from "../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 const VideoTitle = ({ title, overview }) => {
+  const selectedLanguage = useSelector((store) => store.config.language);
+
   return (
     <div className="w-screen aspect-video pt-[20%] px-8 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
       <div>
         <button className="bg-white text-black text-xl p-4 px-12 rounded-lg hover:bg-opacity-90">
-          ▶️ Play
+          ▶️ {searchLanguageConstants[selectedLanguage].playBtn}
         </button>
         <button className="mx-4 bg-gray-500 text-white text-xl p-4 px-12 rounded-lg bg-opacity-75 hover:bg-opacity-90">
-          Add More
+          {searchLanguageConstants[selectedLanguage].addMoreBtn}
         </button>
       </div>
     </div>
