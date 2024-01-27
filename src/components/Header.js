cae4c92be71f5e4730b0336bc6c12012b945349e
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-01-27 11:19:29 +0530

    Add GPT Search button and remove console.log statements

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
+import { toggleSearch } from "../utils/gptSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -42,22 +43,30 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const toggleGptSearch = () => {
+    dispatch(toggleSearch());
+  };
+
   return (
     <div className="absolute w-screen px-8 py-5 bg-gradient-to-b from-black z-10 flex justify-between">
       <img width="200" src={LOGO} alt="logo" />
       {userData.user && (
         <div className="flex">
+          <button
+            className="mr-2 bg-purple-800 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-purple-900"
+            onClick={toggleGptSearch}
+          >
+            GPT Search
+          </button>
           <img
             src={userData?.user?.photoURL}
             alt="usericon"
             width="40"
             className="rounded-full"
           />
-          <div className="px-2">
-            <p className="font-bold">{userData?.user?.displayName}</p>
-            <button className="text-red-500 font-bold" onClick={handleSignOut}>
-              (Sign Out)
-            </button>
+          <div className="px-2 text-white">
+            <p>{userData?.user?.displayName}</p>
+            <button onClick={handleSignOut}>(Sign Out)</button>
           </div>
         </div>
       )}
